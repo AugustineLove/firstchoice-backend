ebad01ce9f2efa423da6e725d75acfd558b035f2
AugustineLove <augustinelovestephens@gmail.com>
2026-07-18 13:47:37 +0000

    Fixed controller undefined issue

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -10,7 +10,7 @@ authRoutes.post('/register', AuthController.register);
 authRoutes.post('/login', AuthController.login);
 authRoutes.post('/refresh', AuthController.refresh);
 authRoutes.get('/me', authenticate, AuthController.me);
-authRoutes.post('/forgot-password', AuthController.forgotPassword);
-authRoutes.post('/sync-reset-password', AuthController.syncResetPassword);
+// authRoutes.post('/forgot-password', AuthController.forgotPassword);
+// authRoutes.post('/sync-reset-password', AuthController.syncResetPassword);
 
 export default authRoutes;
