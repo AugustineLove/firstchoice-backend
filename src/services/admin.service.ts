a63b80f2aa3a13342437142fb55731165cc17163
AugustineLove <augustinelovestephens@gmail.com>
2026-07-24 01:49:47 +0000

    Changed admin service

--- src/services/admin.service.ts
+++ src/services/admin.service.ts
@@ -224,7 +224,7 @@ export async function assignRiderToOrder(orderId: string, riderId: string) {
   const order = await prisma.order.findUnique({ where: { id: orderId } });
   if (!order) throw new Error('Order not found');
   if (order.orderStatus !== 'PENDING')
-    throw new Error('Order must be READY_FOR_PICKUP before assigning a rider');
+    throw new Error('Order must be  before assigning a rider');
 
   const rider = await prisma.rider.findUnique({ where: { id: riderId } });
   if (!rider) throw new Error('Rider not found');
