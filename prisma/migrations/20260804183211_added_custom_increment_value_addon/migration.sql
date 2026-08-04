76533ba63e47166f46d15a6b8547d0992e659241
AugustineLove <augustinelovestephens@gmail.com>
2026-08-04 18:37:35 +0000

    Changed addon-body

---
+++ prisma/migrations/20260804183211_added_custom_increment_value_addon/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "ProductAddon" ADD COLUMN     "customIncrementValue" DOUBLE PRECISION;
+
+-- AlterTable
+ALTER TABLE "ProductAddonGroup" ADD COLUMN     "customIncrementValue" DOUBLE PRECISION;
